9b9b0d743c61a04297e59be4153c503ef48e5b72
ghazie754 <67240493+ghazie754@users.noreply.github.com>
2021-04-28 22:35:32 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword(123);
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+}
+
+function generatePassword(passwordLength) {
+    var numberChars = "0123456789";
+    var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowerChars = "abcdefghiklmnopqrstuvwxyz";
+    var lowerChars = "abcdefghiklmnopqrstuvwxyz";
+    var allChars = numberChars + upperChars + lowerChars;
+    var randPasswordArray = Array(passwordLength);
+    randPasswordArray[0] = numberChars;
+    randPasswordArray[1] = upperChars;
+    randPasswordArray[2] = lowerChars;
+    randPasswordArray = randPasswordArray.fill(allChars, 3);
+    return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
