e6de42c7c7f7b8e15bb038389a4c5a2374193c5e
ghazie754 <67240493+ghazie754@users.noreply.github.com>
2021-05-02 23:39:50 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword(123);
+    var password = generatePassword(145);
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
@@ -14,13 +14,14 @@ function generatePassword(passwordLength) {
     var numberChars = "0123456789";
     var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lowerChars = "abcdefghiklmnopqrstuvwxyz";
-    var lowerChars = "abcdefghiklmnopqrstuvwxyz";
-    var allChars = numberChars + upperChars + lowerChars;
+    var SpecialCharacters = "{}[] += <;>./?*^:;%#@!|-";
+    var allChars = numberChars + upperChars + SpecialCharacters + lowerChars;
     var randPasswordArray = Array(passwordLength);
     randPasswordArray[0] = numberChars;
     randPasswordArray[1] = upperChars;
     randPasswordArray[2] = lowerChars;
-    randPasswordArray = randPasswordArray.fill(allChars, 3);
+    randPasswordArray[3] = SpecialCharacters;
+    randPasswordArray = randPasswordArray.fill(allChars, 5);
     return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
 }
 
